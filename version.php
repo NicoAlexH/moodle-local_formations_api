e16832539c4edd460bde10acabf7584e400e8f07
Nicolas Alexandropoulos <nicolas.alexandropoulos@unil.ch>
2022-03-14 17:25:43 +0100

    Added missing service function

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2021102701';
+$plugin->version = '2021102704';
 $plugin->requires = '2020061500';
 $plugin->component = 'local_formationsapi';
 $plugin->maturity = MATURITY_STABLE;
